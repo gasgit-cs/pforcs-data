6cb7769fa6278d18ce31284a4544c75335df2f2c
gasgit-cs <G00305555@gmit.ie>
2022-01-10 23:08:02 +0000

    update

--- Untitled-1.ps1
+++
@@ -1,3 +0,0 @@
-$userUPN="first.last@gretb.ie"
-$groupName="Create Teams Group"
-Add-AzureADGroupMember -RefObjectId (Get-AzureADUser | Where { $_.UserPrincipalName -eq $userUPN }).ObjectID -ObjectId (Get-AzureADGroup | Where { $_.DisplayName -eq $groupName }).ObjectID
